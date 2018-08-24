5e8e3f03a73d3862499b16c7cc73784947ed585b
k-takami <belltakami@gmail.com>
2018-08-24 10:13:39 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -1,5 +1,5 @@
 # .gitignore
-run 'gibo OSX Ruby Rails JetBrains > .gitignore' rescue nil
+# run 'gibo OSX Ruby Rails JetBrains > .gitignore' rescue nil
 gsub_file '.gitignore', /^config\/initializers\/secret_token.rb\n/, ''
 gsub_file '.gitignore', /^config\/secrets.yml\n/, ''
 
